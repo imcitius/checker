e88bda758223f9bb91c7e7b77ab2dba1523406a0
Alexander Zuenkov <theinpu@gmail.com>
2020-03-16 22:16:00 +0300

    Update main.go

--- main.go
+++ main.go
@@ -19,29 +19,6 @@ func main() {
 	} else {
 		fmt.Println("Start dev ")
 	}
-	//var timeout time.Duration
-	//var timer *time.Timer
-	//var ticker = time.NewTicker(time.Second * 30)
-
-	/*var err error
-
-	fail := false
-	if e, ok := os.LookupEnv("GUIDE_FAIL"); ok {
-		fail = e == "true"
-	}
-
-	useTimer := false
-	if e, ok := os.LookupEnv("GUIDE_RUN_TIME"); ok {
-		useTimer = true
-		timeout, err = time.ParseDuration(e)
-		if err != nil {
-			useTimer = false
-		}
-	}
-
-	if useTimer {
-		timer = time.NewTimer(timeout)
-	}*/
 
 	stopSignal := make(chan os.Signal)
 	signal.Notify(stopSignal, syscall.SIGTERM)
@@ -58,61 +35,6 @@ func main() {
 	}
 
 	http.HandleFunc("/healthcheck", healthcheck)
-
-	/*go func() {
-		if useTimer {
-			for {
-				select {
-				case t := <-ticker.C:
-					fmt.Printf("working %d\n", t.Unix())
-					break
-				case <-timer.C:
-					fmt.Println("time to exit")
-					stopCh <- true
-					break
-				case <-stopSignal:
-					fmt.Println("graceful exit")
-					stopCh <- true
-					break
-				case <-stopCh:
-					var status string
-					if fail {
-						status = "fail"
-					} else {
-						status = "success"
-					}
-					fmt.Printf("exit with %s\n", status)
-					ticker.Stop()
-					timer.Stop()
-					if fail {
-						os.Exit(1)
-					} else {
-						os.Exit(0)
-					}
-				}
-			}
-		} else {
-			if fail {
-				ticker.Stop()
-				os.Exit(2)
-			}
-			for {
-				select {
-				case t := <-ticker.C:
-					fmt.Printf("working %d\n", t.Unix())
-					break
-				case <-stopSignal:
-					fmt.Println("graceful exit")
-					stopCh <- true
-					break
-				case <-stopCh:
-					ticker.Stop()
-					fmt.Println("exit")
-					os.Exit(0)
-				}
-			}
-		}
-	}()*/
 
 	go func() {
 		for {
