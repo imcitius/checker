b39806dfdea9f23ff79781c1126c5c99219d3303
ilya <ilya@2cit.ru>
2020-03-16 22:16:08 +0300

    files load refactor

--- main.go
+++ main.go
@@ -3,30 +3,40 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
 type Config struct {
 	Secs     int    `json:"secs"`
 	BotToken string `json:"bot_token"`
 }
+type urlCheck struct {
+	URL     string `json:"URL"`
+	Channel int    `json:"channel"`
+}
 
 var config Config
+var probes []urlCheck
 
-func main() {
-
-	configFile, err := os.Open("config.json")
-	defer configFile.Close()
-
+func jsonLoad(fileName string, destination interface{}) error {
+	configFile, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err.Error())
-		return
+		return err
+	}
+	err = json.Unmarshal(configFile, &destination)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
 	}
 
-	jsonParser := json.NewDecoder(configFile)
-	jsonParser.Decode(&config)
+	return nil
+}
+
+func main() {
+	jsonLoad("config.json", &config)
+	jsonLoad("data.json", &probes)
 
 	go runListenBot(config.BotToken)
 	urlChecks(config.BotToken)
-
 }
--- urlChecks.go
+++ urlChecks.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"time"
 )
 
-type urlCheck struct {
-	URL     string `json:"URL"`
-	Channel int    `json:"channel"`
-}
-
 func urlChecks(token string) {
-	probes := []urlCheck{}
 	for {
 		<-time.After(time.Duration(config.Secs) * time.Second)
-
-		data, err := ioutil.ReadFile("data.json")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		err = json.Unmarshal(data, &probes)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
 
 		testurls(probes)
 	}
