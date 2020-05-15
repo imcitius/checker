ea495ab0b3a24731d6029cbdf5b697d365c3c710
ilya <ilya@2cit.ru>
2020-05-15 22:21:20 +0300

    add CI_COMMIT_SHORT_SHA into CI version

---
+++ web.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/healthcheck" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	io.WriteString(w, "Ok!\n")
+}
+
+func WebInterface() {
+	var addr string = fmt.Sprintf(":%s", Config.Defaults.HTTPPort)
+
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/healthcheck", healthCheck)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("ListenAndServe: %s", err)
+	}
+}
