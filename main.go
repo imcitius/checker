d658e8dcf4fd48ba773f73ff7d9c9b10add8e7f6
ilya <ilya@2cit.ru>
2020-05-22 17:30:05 +0300

    full refactor to Viper/Cobra

--- main.go
+++ main.go
@@ -1,59 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"github.com/sirupsen/logrus"
-	"math/rand"
-	"time"
+	"my/checker/cmd"
 )
 
-var log = logrus.New()
-
 func main() {
-
-	configPath := flag.String("config", "config.json", "Config file path")
-	debugLevel := flag.String("debug", "Info", "Debug,Info,Warn,Error,Fatal,Panic")
-	botsEnabled := flag.String("bots", "enabled", "enabled/disabled")
-	flag.Parse()
-
-	dl, err := logrus.ParseLevel(*debugLevel)
-	if err != nil {
-		log.Panicf("Cannot parse debug level: %v", err)
-	} else {
-		log.SetLevel(dl)
-	}
-
-	log.Infof("Config file path: %s", *configPath)
-
-	err = jsonLoad(*configPath, &Config)
-	if err != nil {
-		log.Panicf("Config load error: %v", err)
-	} else {
-		err = fillDefaults()
-		if err != nil {
-			panic(err)
-		}
-		fillUUIDs()
-		if err != nil {
-			panic(err)
-		}
-		fillTimeouts()
-
-	}
-
-	if *debugLevel == "Debug" {
-		conf, _ := json.Marshal(Config)
-		log.Debugf("Config: %+v\n\n", string(conf))
-	}
-
-	rand.Seed(time.Now().UnixNano())
-
-	go webInterface()
-
-	if *botsEnabled == "enabled" {
-		initBots()
-	}
-	runScheduler()
-
+	cmd.Execute()
 }
