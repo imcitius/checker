41a627f072aaa82ac0403b66fe41c36d0c00bd64
ilya <ilya@2cit.ru>
2020-07-03 22:00:01 +0300

    add watch services from consul catalog

---
+++ catalog/check.go
@@ -0,0 +1,61 @@
+package catalog
+
+import (
+	"fmt"
+	"my/checker/alerts"
+	checks "my/checker/checks"
+	"my/checker/common"
+	"my/checker/config"
+	"my/checker/metrics"
+	"my/checker/status"
+	"time"
+)
+
+func CheckCatalog(timeout string) {
+
+	//config.Log.Infof("Catalog: %+v", config.ProjectsCatalog)
+
+	for _, p := range config.ProjectsCatalog {
+		for _, h := range p.Healthchecks {
+			for _, c := range h.Checks {
+				if timeout == h.Parameters.RunEvery || timeout == p.Parameters.RunEvery {
+
+					startTime := time.Now()
+					config.Log.Debugf("check: %+v", c)
+					tempErr := checks.Execute(&p, &c)
+					endTime := time.Now()
+					t := endTime.Sub(startTime)
+					evaluateCheckResult(&p, &h, &c, tempErr, common.GetRandomId(), t)
+				}
+			}
+		}
+	}
+}
+
+func evaluateCheckResult(p *config.Project, h *config.Healthcheck, c *config.Check, tempErr error, checkRandomId string, t time.Duration) {
+	if tempErr != nil {
+		err := fmt.Errorf("(%s) %s", checkRandomId, tempErr.Error())
+		config.Log.Infof("(%s) failure: %+v, took %d millisec\n", checkRandomId, err, t.Milliseconds())
+		//config.Log.Debugf("Check mode: %s", status.GetCheckMode(check))
+		if status.GetCheckMode(c) != "quiet" {
+			alerts.ProjectAlert(p, err)
+		}
+
+		status.Statuses.Projects[p.Name].SeqErrorsCount++
+		status.Statuses.Checks[c.UUid].LastResult = false
+
+		err = metrics.AddCheckError(p, h, c)
+		if err != nil {
+			config.Log.Errorf("Metric count error: %v", err)
+		}
+
+	} else {
+		config.Log.Infof("(%s) success, took %d millisec\n", checkRandomId, t.Milliseconds())
+		metrics.CheckDuration.WithLabelValues(p.Name, h.Name, c.UUid, c.Type).Set(float64(t.Milliseconds()))
+
+		status.Statuses.Projects[p.Name].SeqErrorsCount--
+
+		status.Statuses.Projects[p.Name].Alive++
+		status.Statuses.Checks[c.UUid].LastResult = true
+	}
+}
